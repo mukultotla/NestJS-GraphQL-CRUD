78f3016baa9c26f64c41885e35b0940adb5ebf4c
Mukul <mukultotla4@gmail.com>
2023-04-05 22:46:20 +0530

    Added service logic

---
+++ src/book/book.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@nestjs/common';
+import { BookEntity } from './entity/book.entity';
+
+@Injectable()
+export class BookService {
+    public bookData: BookEntity[] = [];
+    
+    addBook(book: BookEntity): string {
+        this.bookData.push(book);
+        return 'Book added successfully';
+    }
+
+    updateBook(id: number, updateBook: BookEntity): string {
+        for(let index = 0; index < this.bookData.length; index++){
+            if(this.bookData[index].id == id){
+                this.updateBook[index] = updateBook;
+            }
+        }
+        return 'Book updated successfully';
+    }
+
+    deleteBook(id: number): string {
+        this.bookData = this.bookData.filter((book) => book.id !== id);
+        return 'Book has been deleted';
+    }
+
+    findBookById(id: number): BookEntity {
+        for(let index = 0; index < this.bookData.length; index++){
+            if(this.bookData[index].id == id){
+                return this.bookData[id];
+            }
+        }
+    }
+
+    findAllBooks(): BookEntity[] {
+        return this.bookData;
+    }
+}
